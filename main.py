c19918a83b436f2a2854e4719b24cc44b42f87ba
santhosh kumar <santhosh16022003@gmail.com>
2022-04-13 16:05:59 +0530

    new

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
+import json
 import jsonschema
 from jsonschema import validate
 
 employeeschema={
+"$schema": "http://json-schema.org/draft-04/schema#",
     "type": "object",
     "properties": {
         "employee": {
@@ -27,7 +29,7 @@ employeeschema={
                             "type": "integer"
                         },
                         "govt id": {
-                            "type": "string"
+                            "type": "integer"
                         },
                         "birth date": {
                             "type": "string"
@@ -44,49 +46,17 @@ employeeschema={
                 }
 ]}}}
 
-def validation(jsonData):
+def validation(Data):
     try:
-        validate(instance=jsonData,schema=employeeschema)
+        validate(instance=Data,schema=employeeschema)
     except jsonschema.exceptions.ValidationError as err:
         return False
     return True
 
-InvalidJsonData = """
-    "employee":
-           [
-		   {"id":1,
-		   "name":"kumar",
-		   "cell phone":1233455554,
-		   "birth date":"16/02/2003",
-            "leave taken":"MO"
-           }{"id":2,
-		   "name":"santhosh",
-		   "home phone":1233765554,
-		   "birth date":"15/01/2001",
-			"leave taken":
-             }
-            ]
-}"""
-isValid = validation(InvalidJsonData)
-
-print("Given JSON string is Valid", isValid)
-
-validJsonData = """{   
-    "employee":
-           [
-		   {"id":1,
-		   "name":"kumar",
-		   "cell phone":1233455554,
-		   "birth date":"16/02/2003",
-            "leave taken":"MO"
-           },{"id":2,
-		   "name":"santhosh",
-		   "home phone":1233765554,
-		   "birth date":"15/01/2001",
-			"leave taken":"SO"
-             }
-            ]
-}"""
-isValid = validation(validJsonData)
-
-print("Given JSON string is Valid", isValid)
+file=open('data.json')
+validdata=json.load(file)
+print(validation(validdata))
+file.close()
+file1=open('invaliddata.json')
+invaliddata=json.load(file1)
+print(validation(invaliddata))
